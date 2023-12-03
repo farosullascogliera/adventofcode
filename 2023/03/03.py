c22b4d3ad4a8a84cd3d86de34b71b53ec412a657
farosullascogliera <r.faro@protonmail.com>
2023-12-03 21:25:34 +0100

    unfinished

---
+++ 2023/03/03.py
@@ -0,0 +1,20 @@
+def gear_ratios_first_puzzle(input) -> int:
+    output = 0
+
+    for line in input:
+        chart = {}
+        
+
+    output = chart
+    return output
+
+
+def gear_ratios_second_puzzle(input) -> int:
+    pass
+
+
+with open("input.txt") as f:
+    input = f.read().splitlines()
+
+print(f"First: {gear_ratios_first_puzzle(input)}")
+# print(f"Second: {gear_ratios_second_puzzle(input)}")
